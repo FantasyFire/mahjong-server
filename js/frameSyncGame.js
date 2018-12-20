3e239bf442aff3a65aa0b1a9ec8a151a242fcd1c
FantasyFire <353676041@qq.com>
2018-12-20 19:59:44 +0800

    开坑准备写帧同步游戏试试

---
+++ js/frameSyncGame.js
@@ -0,0 +1,22 @@
+const Game = require('./game.js');
+const util = require('util');
+
+var FrameSyncGame = function () {
+    Game.call(this);
+    
+};
+
+FrameSyncGame.prototype = {
+    reset () {
+        this.turnCount = 0;
+        this.currentPlayerId = this.playerSequence[0];
+    },
+    getNextPlayerId () {
+        return this.playerSequence[(this.playerSequence.indexOf(this.currentPlayer)+1)%this.playerSequence.length];
+    }
+}
+
+
+util.inherits(FrameSyncGame, Game);
+
+module.exports = TurnBasedGame;
