ef89b053fd426abebee13af514aaa4435564a441
FantasyFire <353676041@qq.com>
2019-07-22 16:25:58 +0800

    先简单实现查询用户

--- js/mongodb.js
+++ js/mongodb.js
@@ -1,11 +1,14 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
-
-// Connection URL
-const url = 'mongodb://$[username]:$[password]@$[hostlist]/$[database]?authSource=$[authSource]';
-
+const config = require('../config.json');
+let client = null;
 // Use connect method to connect to the Server
-MongoClient.connect(url, function (err, client) {
+MongoClient.connect(config.mongodb.adminUrl, function (err, _client) {
     assert.equal(null, err);
-    client.close();
+    client = _client;
 });
+
+exports.findUser = async (name) => {
+    let res = await client.db('admin').collection('user').findOne({name});
+    return res;
+};
--- js/user.router.js
+++ js/user.router.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const db = require('./mongodb');
 
-router.get('/get_info', (req, res, next) => {
-    res.send(`{"msg":"ok"}`);
+router.get('/get_info', async (req, res, next) => {
+    let user = await db.findUser(req.query.name);
+    res.send(user);
 });
 
 module.exports = router;
